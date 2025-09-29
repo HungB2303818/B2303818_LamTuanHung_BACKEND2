7dfe3849bd1ff466e3e3d8201dbcb1727478dfbd
Dill-Doe-James <hungb2303818@student.ctu.edu.vn>
2025-09-29 14:22:13 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const config = require("./app/config");
 
 //start server
 const PORT = config.app.port;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
