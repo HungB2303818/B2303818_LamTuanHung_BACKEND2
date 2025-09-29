02ca87ce05e368faf88a54033e951f6ba53dafa8
Dill-Doe-James <hungb2303818@student.ctu.edu.vn>
2025-09-29 14:33:10 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -8,6 +9,16 @@ const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) =>{
+  return next(new ApirError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) =>{
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
