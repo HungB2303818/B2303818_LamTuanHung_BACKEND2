5476dd02007e93cd15bb08bd0f691664b52bea2d
HungB2303818 <hungb2303818@student.ctu.edu.vn>
2025-10-16 07:51:05 +0700

    Update API

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const cors = require("cors");
 const ApiError = require("./app/api-error");
@@ -9,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
@@ -19,8 +24,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
-});
 
 module.exports = app;
